c23a06ee1769bdabe71fb2c330ed9ac8c6be6a19
kol4egraf0-0 <genitya408@gmail.com>
2025-06-02 23:09:12 +0300

    update activivties+add some util

--- app/src/main/java/com/example/qrcodegen/MainTeacher.java
+++ app/src/main/java/com/example/qrcodegen/MainTeacher.java
@@ -8,11 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SignUpActivity extends AppCompatActivity {
+public class MainTeacher extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_main_teacher);
     }
 }
